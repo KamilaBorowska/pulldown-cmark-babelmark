971f1cd107db6fcb4709e6d4a541f558443e9d07
Konrad Borowski <konrad@borowski.pw>
2019-05-17 08:41:35 +0200

    Update pulldown-cmark to 0.5.1

--- src/main.rs
+++ src/main.rs
@@ -25,7 +25,7 @@ fn main() {
             html::push_html(&mut html, parser);
             warp::reply::json(&Output {
                 name: "pulldown-cmark",
-                version: "0.5.0",
+                version: "0.5.1",
                 html,
             })
         })
